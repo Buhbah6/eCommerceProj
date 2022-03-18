bb13df0039b83f584a909c4a04411b295b23bcdd
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-03-18 15:49:26 -0400

    Fixed UI for login

--- htdocs/app/views/User/login.php
+++ htdocs/app/views/User/login.php
@@ -3,7 +3,6 @@
 		<!-- CSS only -->
 	<link rel="stylesheet" href="../public/css/login.css">
 		<!-- JavaScript Bundle with Popper -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 		<title>User login</title>
 	</head>
 	<body>
@@ -11,14 +10,14 @@
 
 		<h1>Login Account</h1>
 		<form method='post' action=''>
-			<div class="txt-field">
-			<span></span>	
-			<label class='form-label'>Username:<input type='text' name='username'  required /></label><br>
-			</div>
-			<div class="txt-field">
-			<span></span>	
-			<label class='form-label'>Password:<input type='password' name='password' required /></label><br>
-			</div>
+			
+		<span></span>		
+		Username<label class='form-label'><input type='text' name='username'  required /></label>
+			
+			
+				
+		Password<label class='form-label'><input type='password' name='password' required /></label></span><br>
+			
 			
 			<input type="submit" name='action' value='Login!'  />
 			
